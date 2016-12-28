93ebaef2b052825adbcd386997ea2dc28f003dd5
keigo135 <keigo.nishida@hotmail.com>
2016-12-28 12:31:00 -0800

    dec28

--- keigoJanken/js/bubble.js
+++ keigoJanken/js/bubble.js
@@ -1,19 +1,17 @@
 var numbers = [5, 4, 18, 15, 11]
 
-        function bubbleSort(numbers) {
-            var swapped;
-            do {
-                swapped = false;
-                for (var i = 0; i < numbers.length - 1; i++) {
-                    if (numbers[i] > numbers[i + 1]) {
-                        var temp = numbers[i];
-                        numbers[i] = numbers[i + 1];
-                        numbers[i + 1] = temp;
-                        swapped = true;
-                    }
-                }
-            } while (swapped);
+function bubbleSort(numbers) {
+    for (i = 0; i < numbers.length - 1; i++) {
+        for (j = 0; j < numbers.length - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                console.log(i);
+                n = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = n;
+            }
         }
+    }
+}
 
-        bubbleSort(numbers);
-        console.log(numbers);
+bubbleSort(numbers);
+console.log(numbers);
