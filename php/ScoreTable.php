438aec4127049a2de807a62c20f6eee475bb86dc
alien-house <info@alien-house.com>
2017-01-12 12:19:18 -0800

    add isset

--- php/ScoreTable.php
+++ php/ScoreTable.php
@@ -1,14 +1,14 @@
 <?php
 $connection = mysqli_connect('127.0.0.1','root','admin',"janken") or die("Error " . mysqli_error($connection));
 
-if((isset($_POST['janken_name']) || $_POST['janken_name'] === '') && (isset($_POST['janken_token']) || $_POST['janken_token'] === '')){
-	$sql = "SELECT name, score FROM new_score where (name = ".$_POST['janken_name']." and token = ". $_POST['janken_token'] ." )";
-}else{
+if((!isset($_POST['janken_name']) || $_POST['janken_name'] === '') && (!isset($_POST['janken_token']) || $_POST['janken_token'] === '')){
 	$sql = "SELECT name, score, (select count(DISTINCT score) 
 	FROM new_score b WHERE a.score < b.score) + 1 rank 
 	FROM new_score a 
 	ORDER BY rank ASC 
 	limit 100";
+}else{
+	$sql = "SELECT name, score FROM new_score where (name = ".$_POST['janken_name']." and token = ". $_POST['janken_token'] ." )";
 }
 $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 # check result
